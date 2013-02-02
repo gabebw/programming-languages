53f5c7972f8e49f5a0452401f3146bd649cdc93a
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-02-02 00:18:13 -0500

    Go: variables

---
+++ go/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+    var a string = "initial"
+    fmt.Println(a)
+
+    var b, c int = 1, 2
+    fmt.Println(b, c)
+
+    var d = true
+    fmt.Println(d)
+
+    var e int
+    fmt.Println(e)
+
+    // := is shorthand for declaring and initializing a variable,
+    // e.g. for var f string = "short" in this case.
+    f := "short"
+    fmt.Println(f)
+}
