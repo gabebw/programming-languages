254c0b4cebaf46c5a5b4d9d14ff486fd52a8380c
Gabe Berke-Williams <gabe@thoughtbot.com>
2013-02-02 10:59:09 -0500

    go fmt *

--- go/constants.go
+++ go/constants.go
@@ -7,16 +7,16 @@ import "math"
 const s string = "constant"
 
 func main() {
-    fmt.Println(s)
+	fmt.Println(s)
 
-    const n = 500000000
+	const n = 500000000
 
-    const d = 3e20 / n
-    fmt.Println(d)
+	const d = 3e20 / n
+	fmt.Println(d)
 
-    // a numeric constant has no type until it's given one (here, it's cast)
-    fmt.Println(int64(d))
+	// a numeric constant has no type until it's given one (here, it's cast)
+	fmt.Println(int64(d))
 
-    // give   n a type by using it in a context that requires one
-    fmt.Println(math.Sin(n))
+	// give   n a type by using it in a context that requires one
+	fmt.Println(math.Sin(n))
 }
--- go/variables.go
+++ go/variables.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 func main() {
-    var a string = "initial"
-    fmt.Println(a)
+	var a string = "initial"
+	fmt.Println(a)
 
-    var b, c int = 1, 2
-    fmt.Println(b, c)
+	var b, c int = 1, 2
+	fmt.Println(b, c)
 
-    var d = true
-    fmt.Println(d)
+	var d = true
+	fmt.Println(d)
 
-    var e int
-    fmt.Println(e)
+	var e int
+	fmt.Println(e)
 
-    // := is shorthand for declaring and initializing a variable,
-    // e.g. for var f string = "short" in this case.
-    f := "short"
-    fmt.Println(f)
+	// := is shorthand for declaring and initializing a variable,
+	// e.g. for var f string = "short" in this case.
+	f := "short"
+	fmt.Println(f)
 }
